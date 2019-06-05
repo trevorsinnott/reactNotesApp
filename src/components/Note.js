44a16462a13b0bfc4c9e684c800431f4152613d8
Trevor <sinnott.trevor@gmail.com>
2019-06-06 08:50:13 +0900

    clean up formatting and remove unused dependancies

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,53 +4,52 @@ import DisplayNote from "./DisplayNote";
 import TextField from "@material-ui/core/TextField";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
-import Divider from '@material-ui/core/Divider';
+import Divider from "@material-ui/core/Divider";
 import Box from "@material-ui/core/box";
-
 
 function Note(props) {
   return (
     <Box my={2}>
-    <Card >
-      <CardHeader
-        subheader={props.data.created}
-        action={
-          <DeleteNoteButton
-            handleSubmit={props.handleSubmit}
+      <Card>
+        <CardHeader
+          subheader={props.data.created}
+          action={
+            <DeleteNoteButton
+              handleSubmit={props.handleSubmit}
+              keyNr={props.keyNr}
+            />
+          }
+        >
+          <span name="created" htmlFor="title">
+            Created:{" "}
+            <time dateTime={props.data.created}>{props.data.created}</time>
+          </span>
+        </CardHeader>
+        <Divider variant="inset" />
+        {props.data.canEdit ? (
+          <form onBlur={() => props.toggleDisplay(props.keyNr, false)}>
+            <TextField
+              fullWidth={true}
+              multiline={true}
+              variant="outlined"
+              autoFocus={true}
+              onClick={() => props.toggleDisplay(props.keyNr, true)}
+              name="body"
+              value={props.data.body}
+              onChange={event => {
+                props.handleChange(event, props.keyNr);
+              }}
+            />
+          </form>
+        ) : (
+          <DisplayNote
+            data={props.data}
             keyNr={props.keyNr}
-          />
-        }
-      >
-        <span name="created" htmlFor="title">
-          Created:{" "}
-          <time dateTime={props.data.created}>{props.data.created}</time>
-        </span>
-      </CardHeader>
-      <Divider variant="inset" />
-      {props.data.canEdit ? (
-        <form onBlur={() => props.toggleDisplay(props.keyNr, false)}>
-          <TextField
-            fullWidth={true}
-            multiline={true}
-            variant="outlined"
-            autoFocus={true}
+            toggleDisplay={props.toggleDisplay}
             onClick={() => props.toggleDisplay(props.keyNr, true)}
-            name="body"
-            value={props.data.body}
-            onChange={event => {
-              props.handleChange(event, props.keyNr);
-            }}
           />
-        </form>
-      ) : (
-        <DisplayNote
-          data={props.data}
-          keyNr={props.keyNr}
-          toggleDisplay={props.toggleDisplay}
-          onClick={() => props.toggleDisplay(props.keyNr, true)}
-        />
-      )}
-    </Card>
+        )}
+      </Card>
     </Box>
   );
 }
--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import NewNoteButton from "./NewNoteButton";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -24,8 +23,7 @@ function TopAppBar(props) {
           <Typography variant="h6" className={classes.title}>
             Notes
           </Typography>
-            <NewNoteButton createNote={props.createNote} />
-
+          <NewNoteButton createNote={props.createNote} />
         </Toolbar>
       </AppBar>
     </div>
