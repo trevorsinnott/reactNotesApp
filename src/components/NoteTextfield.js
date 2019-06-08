0c991aadc09c8579c81b60bbdedc2b5b2b2d176d
Trevor <sinnott.trevor@gmail.com>
2019-06-08 16:41:52 +0900

    replace onBlur action with ClickAwayListener in NoteTextfield

--- src/components/NoteTextfield.js
+++ src/components/NoteTextfield.js
@@ -1,35 +1,49 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import { CardContent, Divider, Button, CardActions } from "@material-ui/core";
+import {
+  CardContent,
+  Divider,
+  Button,
+  CardActions,
+  ClickAwayListener
+} from "@material-ui/core";
 import NoteHeader from "./NoteHeader";
 
 function NoteTextfield(props) {
   return (
-    <>
-      <NoteHeader {...props} />
-      <CardContent>
-        <form onBlur={() => props.toggleDisplay(props.keyNr, false)}>
-          <TextField
-            fullWidth={true}
-            multiline={true}
-            variant="outlined"
-            autoFocus={true}
-            onClick={() => props.toggleDisplay(props.keyNr, true)}
-            name="body"
-            value={props.data.body}
-            onChange={event => {
-              props.handleChange(event, props.keyNr);
-            }}
-          />
-        </form>
-      </CardContent>
-      <Divider />
-      <CardActions>
-        <Button variant="contained" size="small">
-          Save
-        </Button>
-      </CardActions>
-    </>
+    <ClickAwayListener
+      onClickAway={() => props.toggleDisplay(props.keyNr, false)}
+    >
+      <div>
+        <NoteHeader {...props} />
+        <CardContent>
+          <form>
+            <TextField
+              fullWidth={true}
+              multiline={true}
+              variant="outlined"
+              autoFocus={true}
+              onClick={() => props.toggleDisplay(props.keyNr, true)}
+              name="body"
+              value={props.data.body}
+              onChange={event => {
+                props.handleChange(event, props.keyNr);
+              }}
+            />
+          </form>
+        </CardContent>
+        <Divider />
+        <CardActions>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={() => props.toggleDisplay(props.keyNr, false)}
+          >
+            Save
+          </Button>
+        </CardActions>
+      </div>
+    </ClickAwayListener>
   );
 }
 
