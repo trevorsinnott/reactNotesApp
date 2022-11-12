3aed40f59c3a3947c1a48e69a220d0cdef70de5b
Trevor Sinnott <sinnott.trevor@gmail.com>
2022-11-11 18:10:38 -0800

    update to latest version of react and react-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.register();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
